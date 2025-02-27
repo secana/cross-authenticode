2931160c5f67fc20ef880b50a01ab55de4e57585
Stefan Hausotte <stefan.hausotte@gmx.de>
2025-02-27 20:52:18 +0100

    compute sha-1 fingerprint

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 #![allow(dead_code)]
 
 use cms::{
-    cert::x509::der::{Decode, SliceReader},
+    cert::x509::der::{Decode, Encode, EncodePem, SliceReader},
     content_info::ContentInfo,
     signed_data::SignedData,
+};
+use crypto::{
+    digest::Digest,
+    sha1::{self, Sha1},
 };
 use object::{LittleEndian, pe::IMAGE_DIRECTORY_ENTRY_SECURITY, read::pe::PeFile64};
 
@@ -58,9 +62,31 @@ fn get_export_dir(data: &[u8]) {
             }
         });
 
+    let mut i = 0;
     for cert in certificates {
+        println!("Certificate {}", i);
         println!("Subject: {}", cert.tbs_certificate.subject);
         println!("Issuer: {}", cert.tbs_certificate.issuer);
+
+        let pem = cert
+            .to_pem(cms::cert::x509::der::pem::LineEnding::LF)
+            .unwrap();
+
+        std::fs::write(format!("cert-{}.pem", i), pem.as_str()).unwrap();
+
+        let mut bytes = Vec::new();
+        cert.encode_to_vec(&mut bytes).unwrap();
+
+        let mut hasher = Sha1::new();
+
+        hasher.input(&bytes);
+
+        let hex = hasher.result_str();
+
+        println!("SHA-1: {}", hex);
+        println!("\n");
+
+        i += 1;
     }
 }
 
@@ -72,6 +98,14 @@ mod tests {
 
     #[test]
     fn test_get_export_dir() {
+        // Cert-0
+        // SHA-1: f55115d2439ce0a7529ffaaea654be2c71dce955
+        // SHA256: 9267a08c9fc07b6ab194dc4df3121b264e825330a39ffc42cdb0942f5115eb97
+        //
+        // Cert-1
+        // SHA-1: 580a6f4cc4e4b669b9ebdc1b2b3e087b80d0678d
+        // SHA256: e8e95f0733a55e8bad7be0a1413ee23c51fcea64b3c8fa6a786935fddcc71961
+
         let pe_path = PathBuf::from("test-pe/test.bin");
         let pe_file = std::fs::read(pe_path).unwrap();
         get_export_dir(&pe_file);
