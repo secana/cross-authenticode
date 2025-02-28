88eb61898dcd022481be4db57501843039ce64cb
Stefan Hausotte <stefan.hausotte@gdata.de>
2025-02-28 10:32:14 +0100

    refactor

--- src/authenticode_certificate.rs
+++ src/authenticode_certificate.rs
@@ -11,25 +11,29 @@ pub struct AuthenticodeCertificate {
 }
 
 impl AuthenticodeCertificate {
-    pub fn new(certificate: Certificate) -> Result<Self, AuthenticodeError> {
-        let (sha1, sha256) = thumbprints(&certificate)?;
+    fn thumbprints(cert: &Certificate) -> Result<(String, String), AuthenticodeError> {
+        let mut bytes = Vec::new();
+        let _ = cert.encode_to_vec(&mut bytes)?;
+
+        let mut sha1_hasher = Sha1::new();
+        sha1_hasher.input(&bytes);
+
+        let mut sha256_hasher = Sha256::new();
+        sha256_hasher.input(&bytes);
+
+        Ok((sha1_hasher.result_str(), sha256_hasher.result_str()))
+    }
+}
+
+impl TryFrom<Certificate> for AuthenticodeCertificate {
+    type Error = AuthenticodeError;
+
+    fn try_from(certificate: Certificate) -> Result<Self, Self::Error> {
+        let (sha1, sha256) = Self::thumbprints(&certificate)?;
         Ok(Self {
             certificate,
             sha1,
             sha256,
         })
     }
-}
-
-fn thumbprints(cert: &Certificate) -> Result<(String, String), AuthenticodeError> {
-    let mut bytes = Vec::new();
-    let _ = cert.encode_to_vec(&mut bytes)?;
-
-    let mut sha1_hasher = Sha1::new();
-    sha1_hasher.input(&bytes);
-
-    let mut sha256_hasher = Sha256::new();
-    sha256_hasher.input(&bytes);
-
-    Ok((sha1_hasher.result_str(), sha256_hasher.result_str()))
 }
--- src/authenticode_info.rs
+++ src/authenticode_info.rs
@@ -16,58 +16,70 @@ pub struct AuthenticodeInfo {
 }
 
 impl AuthenticodeInfo {
-    pub fn new(data: &[u8]) -> Result<Self, AuthenticodeError> {
-        authenticode_info(data)
+    fn create(data: &[u8]) -> Result<AuthenticodeInfo, AuthenticodeError> {
+        let win_certificate = Self::win_certificate(data)?;
+        let signed_data = Self::signed_data(&win_certificate)?;
+        let authenticode_certificates = Self::certificates(signed_data)?;
+
+        Ok(AuthenticodeInfo {
+            certificates: authenticode_certificates,
+        })
+    }
+
+    fn signed_data(win_certificate: &WinCertificate) -> Result<SignedData, AuthenticodeError> {
+        let mut reader = SliceReader::new(win_certificate.certificate)?;
+        let content_info = ContentInfo::decode(&mut reader)?;
+        let signed_data = content_info.content.decode_as::<SignedData>()?;
+        Ok(signed_data)
+    }
+
+    fn win_certificate(data: &[u8]) -> Result<WinCertificate, AuthenticodeError> {
+        // TODO: Support 32-bit PE files
+        let pe = PeFile64::parse(data)?;
+
+        let security_dir = pe
+            .data_directory(IMAGE_DIRECTORY_ENTRY_SECURITY)
+            .ok_or(AuthenticodeError::NoWinCertificate)?;
+        let win_certificate =
+            WinCertificate::new(data, security_dir.virtual_address.get(LittleEndian))?;
+
+        Ok(win_certificate)
+    }
+
+    fn certificates(
+        signed_data: SignedData,
+    ) -> Result<Vec<AuthenticodeCertificate>, AuthenticodeError> {
+        let authenticode_certificates = signed_data
+            .certificates
+            .as_ref()
+            .ok_or(AuthenticodeError::NoCertificates)?
+            .0
+            .iter()
+            .filter_map(|cert| match cert {
+                CertificateChoices::Certificate(cert) => Some(cert),
+                _ => None,
+            })
+            .map(|cert| AuthenticodeCertificate::try_from(cert.to_owned()))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(authenticode_certificates)
     }
 }
 
-fn authenticode_info(data: &[u8]) -> Result<AuthenticodeInfo, AuthenticodeError> {
-    let win_certificate = win_certificate(data)?;
-    let signed_data = signed_data(&win_certificate)?;
-    let authenticode_certificates = certificates(signed_data)?;
+impl TryFrom<&[u8]> for AuthenticodeInfo {
+    type Error = AuthenticodeError;
 
-    Ok(AuthenticodeInfo {
-        certificates: authenticode_certificates,
-    })
+    fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
+        Self::create(data)
+    }
 }
 
-fn signed_data(win_certificate: &WinCertificate) -> Result<SignedData, AuthenticodeError> {
-    let mut reader = SliceReader::new(win_certificate.certificate)?;
-    let content_info = ContentInfo::decode(&mut reader)?;
-    let signed_data = content_info.content.decode_as::<SignedData>()?;
-    Ok(signed_data)
-}
+impl TryFrom<&Vec<u8>> for AuthenticodeInfo {
+    type Error = AuthenticodeError;
 
-fn win_certificate(data: &[u8]) -> Result<WinCertificate, AuthenticodeError> {
-    // TODO: Support 32-bit PE files
-    let pe = PeFile64::parse(data)?;
-
-    let security_dir = pe
-        .data_directory(IMAGE_DIRECTORY_ENTRY_SECURITY)
-        .ok_or(AuthenticodeError::NoWinCertificate)?;
-    let win_certificate =
-        WinCertificate::new(data, security_dir.virtual_address.get(LittleEndian))?;
-
-    Ok(win_certificate)
-}
-
-fn certificates(
-    signed_data: SignedData,
-) -> Result<Vec<AuthenticodeCertificate>, AuthenticodeError> {
-    let authenticode_certificates = signed_data
-        .certificates
-        .as_ref()
-        .ok_or(AuthenticodeError::NoCertificates)?
-        .0
-        .iter()
-        .filter_map(|cert| match cert {
-            CertificateChoices::Certificate(cert) => Some(cert),
-            _ => None,
-        })
-        .map(|cert| AuthenticodeCertificate::new(cert.to_owned()))
-        .collect::<Result<Vec<_>, _>>()?;
-
-    Ok(authenticode_certificates)
+    fn try_from(data: &Vec<u8>) -> Result<Self, Self::Error> {
+        Self::create(data)
+    }
 }
 
 #[cfg(test)]
@@ -80,7 +92,7 @@ mod tests {
         let pe_path = PathBuf::from("test-pe/test.bin");
         let pe_file = std::fs::read(pe_path).unwrap();
 
-        let ai = AuthenticodeInfo::new(&pe_file).unwrap();
+        let ai = AuthenticodeInfo::try_from(&pe_file).unwrap();
 
         assert_eq!(ai.certificates.len(), 2);
         assert_eq!(
